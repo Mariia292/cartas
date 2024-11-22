c4de43c0f7019bde5e1bcd7903e7adf04de500a6
Mariia292 <me962690@gmail.com>
2024-11-22 11:07:04 -0300

    Add files via upload

---
+++ flashcard-aula_10/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'matematica',
+    'O que é fraçao?',
+    'fraçao e uma representaçao matematica que expressa uma parte de um todo'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da inglaterra?',
+    'A capital da inglaterra e londres'
+)
+
+criaCartao(
+    'Programação',
+    'qual a linguagem de programaçao mais utilizada para desenvolvimento de aplicativo nobres?',
+    'java e swift'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz te amo em Inglês?',
+    'i love you'
+)
